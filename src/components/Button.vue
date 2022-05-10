da1456055bec85076f4707db7bbbdeee1f0755b9
Alex <opetrovskyi423@gmail.com>
2022-05-10 22:26:02 +0200

    vue-start todo

---
+++ src/components/Button.vue
@@ -0,0 +1,26 @@
+<template>
+  <button @click="onClick()" :style="{background:color}" class="btn">{{ title }}</button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    title: {
+      type: String,
+      default: 'button',
+    },
+    color: String
+  },
+  methods: {
+    onClick(){
+      console.log('yo')
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,23 @@
+<template>
+  <div :key="task.id" v-for="task in tasks">
+    <Task :task="task" />
+  </div>
+</template>
+
+<script>
+import Task from "@/components/Task";
+
+export default {
+  name: "Tasks",
+  components: {
+    Task,
+  },
+  props: {
+    tasks: Array
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
