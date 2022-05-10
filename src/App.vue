1aee3e97921d19f7c97a84ae09691650f0bbcdec
Alex <opetrovskyi423@gmail.com>
2022-05-10 22:58:28 +0200

    delete props

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <Header title="Task Tracker "/>
-    <Tasks :tasks="tasks" />
+    <Tasks @delete-task="deleteTask"
+           :tasks="tasks" />
   </div>
 </template>
 
@@ -20,6 +21,11 @@ export default {
   data() {
     return {
       tasks: []
+    }
+  },
+  methods: {
+    deleteTask(id) {
+      console.log('task',id)
     }
   },
   created() {
--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="task">
+  <div :class="[task.reminder ? 'reminder': '', 'task']">
     <h3>
       {{ task.text }}
-      <i @click="$emit('delete-task', task.id)" class="fas fa-times"></i>
+      <i @click="onDelete(task.id)"  class="fas fa-times"></i>
     </h3>
     <p>{{ task.day }}</p>
 
@@ -10,10 +10,17 @@
 </template>
 
 <script>
+
+
 export default {
   name: "Task",
   props: {
     task: Object
+  },
+  methods: {
+    onDelete(id){
+      this.$emit('delete-task', id)
+    }
   }
 }
 </script>
